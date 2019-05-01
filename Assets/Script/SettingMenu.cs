e39ccb63c97f3b6a609e74060394dd1eb804b1d3
Hieu Dinh <hiiamaalien@gmail.com>
2019-04-30 20:01:45 -0400

    Update Information Panel

---
+++ Assets/Script/SettingMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer AudioControl;
+    public Slider MasterSlider;
+    public Slider SoundEffectSlider;
+    public Slider BackgroundSlider;
+
+
+    public void SetMasterVolume(float volume)
+    {
+        AudioControl.SetFloat("Master", volume);
+        if (MasterSlider.value == -30)
+        {
+            AudioControl.SetFloat("Master", -80);
+        }
+    }
+
+    public void SetSoundEffectsVolume(float volume)
+    {
+        AudioControl.SetFloat("Sound Effect", volume);
+        if (SoundEffectSlider.value == -30)
+        {
+            AudioControl.SetFloat("Sound Effect", -80);
+        }
+    }
+    public void SetBackgroundVolume(float volume)
+    {
+        AudioControl.SetFloat("Background", volume);
+        if (BackgroundSlider.value == -30)
+        {
+            AudioControl.SetFloat("Background", -80);
+        }
+    }
+}
