df6e943c2548a77a497a9885b0824ecd37be5fae
Hieu Dinh <hiiamaalien@gmail.com>
2019-05-03 23:59:38 -0400

    Remaking Game 5 Finished Left Panel

---
+++ Temp Tres/Assets/GameSettingMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettingMenu : MonoBehaviour
+{
+    public void SetVolumeSlider()
+    {
+
+    }
+    public void ExitGame()
+    {
+
+    }
+}
