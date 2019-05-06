09d52db0f257192d005e1d661067d6c550e45054
Hieu Dinh <hiiamaalien@gmail.com>
2019-05-06 12:03:51 -0400

    Starting working on player event

---
+++ Temp Tres/Assets/Command.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+#pragma warning disable 0618
+
+public class Command : NetworkBehaviour
+{
+    Player myPlayer;
+
+    public void OnEnable()
+    {
+        EventManager.StartListening(E_EventName.Player_Join, UpdatePlayerLocation);
+    }
+    public void OnDisable()
+    {
+        EventManager.StopListening(E_EventName.Player_Join, CommandOnPlayerJoin);
+    }
+    void Start()
+    {
+        myPlayer = GetComponent<Player>();
+    }
+
+
+
+    #region E_EventName.Player_Join
+    private void UpdatePlayerLocation(EventParam param)
+    {
+        CmdUpdatePlayerLocation();
+    }
+    [Command]
+    void CmdUpdatePlayerLocation()
+    {
+        RpcUpdatePlayerLocation();
+    }
+    [ClientRpc]
+    void RpcUpdatePlayerLocation()
+    {
+        if (myPlayer.isLocalPlayer)
+        {
+
+        }
+    }
+    #endregion
+}
--- Temp Tres/Assets/Script/Player.cs
+++ Temp Tres/Assets/Script/Player.cs
@@ -13,15 +13,15 @@ public class Player : NetworkBehaviour
     #endregion
 
     [SyncVar] private string m_PlayerName;
-    [SyncVar] private string m_PlayerColor;
+    [SyncVar] private string m_PlayerWildColor;
     [SyncVar] private int m_PlayerNum, m_PlayerScore;
     [SyncVar] private bool m_MyTurn;
 
-    [SyncVar(hook = "OnChangeTextMessage")]
-    private string m_TextMessage;
+    [SyncVar(hook = "OnChangeChatMessage")]
+    private string m_ChatMessage;
 
 
-    private NetworkManager m_NetworkMan;
+    private NetworkManager m_NetMan;
     private GameObject m_PlayerCommand;
     private GameObject m_PlayerHand;
     private GameObject m_PlayerDebugMessage;
@@ -30,11 +30,50 @@ public class Player : NetworkBehaviour
     public void OnEnable()
     {
         
+
+      
     }
 
     public override void OnStartServer()
     {
-        
+        NetMan = GameObject.Find("Net Man").GetComponent<NetworkManager>();
+        PlayerName = "P" + NetMan.numPlayers;
+        PlayerWildColor = "Red";
+        PlayerNum = NetMan.numPlayers;
+        PlayerScore = 0;
+        MyTurn = false;       
+    }
+
+    private void Start()
+    {
+        NetMan = GameObject.Find("Net Man").GetComponent<NetworkManager>();
+        NetMan.GetComponent<NetworkManagerHUD>().showGUI = false;
+
+        transform.SetParent(GameObject.Find("Player List").transform, false);
+        transform.localPosition = Vector3.zero;
+
+
+        if (isLocalPlayer)
+        {
+
+            LocalPlayer = this;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180);
+            foreach (Transform child in PlayerCommand.transform)
+            {
+                if (child.name == "Play Button" || child.name == "Extra Button" || child.name == "Draw Button" || child.name == "Wild Button")
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        EventObject eventObject = new EventObject();
+        eventObject.TypeString = new List<string>() { PlayerName };
+        EventManager.TriggerEvent(E_EventName.Player_Join, eventObject);
     }
 
 
@@ -46,12 +85,12 @@ public class Player : NetworkBehaviour
 
     #region Getter & Setter
     public string PlayerName { get => m_PlayerName; set => m_PlayerName = value; }
-    public string PlayerColor { get => m_PlayerColor; set => m_PlayerColor = value; }
+    public string PlayerWildColor { get => m_PlayerWildColor; set => m_PlayerWildColor = value; }
     public int PlayerNum { get => m_PlayerNum; set => m_PlayerNum = value; }
     public int PlayerScore { get => m_PlayerScore; set => m_PlayerScore = value; }
     public bool MyTurn { get => m_MyTurn; set => m_MyTurn = value; }
-    public string TextMessage { get => m_TextMessage; set => m_TextMessage = value; }
-    public NetworkManager NetworkMan { get => m_NetworkMan; set => m_NetworkMan = value; }
+    public string ChatMessage { get => m_ChatMessage; set => m_ChatMessage = value; }
+    public NetworkManager NetMan { get => m_NetMan; set => m_NetMan = value; }
     public GameObject PlayerCommand { get => m_PlayerCommand; set => m_PlayerCommand = value; }
     public GameObject PlayerHand { get => m_PlayerHand; set => m_PlayerHand = value; }
     public GameObject PlayerDebugMessage { get => m_PlayerDebugMessage; set => m_PlayerDebugMessage = value; }
